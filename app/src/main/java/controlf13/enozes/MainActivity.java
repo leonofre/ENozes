9703e178e97984964940e25ba937739a59b116ef
leonofre <leonardodias.14.ld@gmail.com>
2017-10-31 14:42:37 -0200

    Adicionada função de sair no btnVoltar e função de vibrar em todos os botões

--- app/src/main/java/controlf13/enozes/MainActivity.java
+++ app/src/main/java/controlf13/enozes/MainActivity.java
@@ -1,13 +1,44 @@
 package controlf13.enozes;
 
+import android.content.Context;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private OnClickListener exit = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+            Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vibe.vibrate(100);
+            System.exit(0);
+        }
+    };
+    private OnClickListener config = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vibe.vibrate(100);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnVoltar = (ImageButton) findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener( this.exit );
+
+        ImageButton btnConfig = (ImageButton) findViewById(R.id.btnConfig);
+        btnConfig.setOnClickListener( this.config );
     }
+
 }
