ef2cb0db62dab6652670579a238f80501ac33491
leonofre <leonardodias.14.ld@gmail.com>
2017-11-02 16:48:31 -0200

    Adicionadas funcionalidades de troca de cor e vibração nos botões de configuração de som e música

--- app/src/main/java/controlf13/enozes/ConfigActivity.java
+++ app/src/main/java/controlf13/enozes/ConfigActivity.java
@@ -1,32 +1,75 @@
 package controlf13.enozes;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.view.View.OnClickListener;
+import java.util.HashMap;
+import java.util.Map;
+
+import controlf13.enozes.Funcionalidades;
 
 public class ConfigActivity extends AppCompatActivity {
+    static public Map<String, Boolean> config = new HashMap<String, Boolean>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_config);
+        config.put( "Sound", true );
+        config.put( "Music", true );
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_config );
 
-        ImageButton btnVoltar = (ImageButton) findViewById(R.id.btnVoltar);
+        ImageButton btnVoltar = ( ImageButton ) findViewById( R.id.btnVoltar );
         btnVoltar.setOnClickListener( this.back() );
+
+        ImageButton soundActive = ( ImageButton ) findViewById( R.id.sound );
+        ImageButton soundInactive = ( ImageButton ) findViewById( R.id.notSound );
+        ImageButton musicActive = ( ImageButton ) findViewById( R.id.music );
+        ImageButton musicInactive = ( ImageButton ) findViewById( R.id.notMusic );
+        soundActive.setOnClickListener( this.setConfig( "Sound", soundActive, soundInactive ) );
+        soundInactive.setOnClickListener( this.setConfig( "Sound", soundActive, soundInactive ) );
+        musicActive.setOnClickListener( this.setConfig( "Music", musicActive, musicInactive ) );
+        musicInactive.setOnClickListener( this.setConfig( "Music", musicActive, musicInactive ) );
     }
 
     protected OnClickListener back() {
         OnClickListener voltar = new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                vibe.vibrate(100);
+                Funcionalidades.vibe(50, getApplicationContext() );
                 finish();
             }
         };
         return voltar;
+    }
+
+    protected OnClickListener setConfig( final String configKey,final ImageButton btnActive, final ImageButton btnInactive ) {
+        OnClickListener setConfig = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if ( ConfigActivity.config.get( configKey ) ) {
+                    ConfigActivity.config.put( configKey, false );
+                    btnActive.setBackgroundColor(Color.parseColor("#21100B"));
+                    btnActive.setImageResource(R.drawable.somiconeinativo);
+                    btnInactive.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    btnInactive.setImageResource(R.drawable.xiconeativo);
+                    Funcionalidades.vibe(50, getApplicationContext() );
+                } else {
+                    ConfigActivity.config.put( configKey, true );
+                    btnActive.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    btnActive.setImageResource(R.drawable.somiconeativo);
+                    btnInactive.setBackgroundColor(Color.parseColor("#21100B"));
+                    btnInactive.setImageResource(R.drawable.xiconeinativo);
+                    Funcionalidades.vibe(50, getApplicationContext() );
+                }
+            }
+        };
+        return setConfig;
     }
 }
