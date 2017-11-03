5eb58e570e72a919fbf9995a399214e086960b34
leonofre <leonardodias.14.ld@gmail.com>
2017-11-03 17:24:12 -0200

    Adicionadas funções para habilitar e desabilitar retorno sonoro

--- app/src/main/java/controlf13/enozes/ConfigActivity.java
+++ app/src/main/java/controlf13/enozes/ConfigActivity.java
@@ -2,6 +2,7 @@ package controlf13.enozes;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,26 +12,33 @@ import android.widget.ImageButton;
 import android.view.View.OnClickListener;
 import java.util.HashMap;
 import java.util.Map;
-
 import controlf13.enozes.Funcionalidades;
 
 public class ConfigActivity extends AppCompatActivity {
-    static public Map<String, Boolean> config = new HashMap<String, Boolean>();
+    static public boolean first = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        config.put( "Sound", true );
-        config.put( "Music", true );
+        if ( first ) {
+            Funcionalidades.setSoundSettings();
+            first = false;
+        }
         getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_config );
-
         ImageButton btnVoltar = ( ImageButton ) findViewById( R.id.btnVoltar );
-        btnVoltar.setOnClickListener( this.back() );
-
         ImageButton soundActive = ( ImageButton ) findViewById( R.id.sound );
         ImageButton soundInactive = ( ImageButton ) findViewById( R.id.notSound );
         ImageButton musicActive = ( ImageButton ) findViewById( R.id.music );
         ImageButton musicInactive = ( ImageButton ) findViewById( R.id.notMusic );
+
+        if ( ! Funcionalidades.config.get( "Sound" ) ) {
+            soundActive.setBackgroundColor(Color.parseColor("#21100B"));
+            soundActive.setImageResource(R.drawable.somiconeinativo);
+            soundInactive.setBackgroundColor(Color.parseColor("#FFFFFF"));
+            soundInactive.setImageResource(R.drawable.xiconeativo);
+        }
+
+        btnVoltar.setOnClickListener( this.back() );
         soundActive.setOnClickListener( this.setConfig( "Sound", soundActive, soundInactive ) );
         soundInactive.setOnClickListener( this.setConfig( "Sound", soundActive, soundInactive ) );
         musicActive.setOnClickListener( this.setConfig( "Music", musicActive, musicInactive ) );
@@ -52,16 +60,22 @@ public class ConfigActivity extends AppCompatActivity {
         OnClickListener setConfig = new OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if ( ConfigActivity.config.get( configKey ) ) {
-                    ConfigActivity.config.put( configKey, false );
+                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                if ( Funcionalidades.config.get( configKey ) ) {
+                    Funcionalidades.config.put( configKey, false );
+                    if ( "Sound" == configKey ) {
+                        audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, 0, 0);
+                    }
                     btnActive.setBackgroundColor(Color.parseColor("#21100B"));
                     btnActive.setImageResource(R.drawable.somiconeinativo);
                     btnInactive.setBackgroundColor(Color.parseColor("#FFFFFF"));
                     btnInactive.setImageResource(R.drawable.xiconeativo);
                     Funcionalidades.vibe(50, getApplicationContext() );
                 } else {
-                    ConfigActivity.config.put( configKey, true );
+                    Funcionalidades.config.put( configKey, true );
+                    if ( "Sound" == configKey ) {
+                        audioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, 10, 0);
+                    }
                     btnActive.setBackgroundColor(Color.parseColor("#FFFFFF"));
                     btnActive.setImageResource(R.drawable.somiconeativo);
                     btnInactive.setBackgroundColor(Color.parseColor("#21100B"));
--- app/src/main/java/controlf13/enozes/Funcionalidades.java
+++ app/src/main/java/controlf13/enozes/Funcionalidades.java
@@ -6,13 +6,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by leona on 02/11/2017.
  */
 
 public class Funcionalidades {
-
+    static public Map<String, Boolean> config = new HashMap<String, Boolean>();
+    static public void setSoundSettings() {
+        config.put( "Sound", true );
+        config.put( "Music", true );
+    }
     /**
      * Função para chamar a classe vibrator.
      *
